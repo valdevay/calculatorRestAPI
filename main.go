eb438642a29ce9b55fa0d60dd3cb1b347a186fe1
Vladimir <valdevay@gmail.com>
2025-07-04 15:25:40 +0300

    main handles

--- main.go
+++ main.go
@@ -1,19 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
 
-type Calcutation struct {
+	"github.com/Knetic/govaluate"
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Calculation struct {
 	ID         string `json:"id"`
 	Expression string `json:"expression"`
 	Result     string `json:"result"`
 }
 
-type CalcutationRequest struct {
+type CalculationRequest struct {
 	Expression string `json:"expression"`
 }
 
-var calculation = []Calcutation{} //nessesary to initilization like a slice
+var calculations = []Calculation{} //nessesary to initilization like a slice
+
+func calculateExpression(expression string) (string, error) {
+	expr, err := govaluate.NewEvaluableExpression(expression) // 7 + 7 for example
+	if err != nil {
+		return "", err // mistake like (5 + a)
+	}
+	result, err := expr.Evaluate(nil)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%v", result), nil
+
+}
+
+func getCalculations(c echo.Context) error {
+	return c.JSON(http.StatusOK, calculations)
+}
+
+func postCalculations(c echo.Context) error {
+	var request CalculationRequest
+	if err := c.Bind(&request); err != nil {
+		return c.JSON(http.StatusBadGateway, map[string]string{"error": "Invalid request"})
+	}
+
+	result, err := calculateExpression(request.Expression)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid expression"})
+	}
+
+	calc := Calculation{
+		ID:         uuid.NewString(),
+		Expression: request.Expression,
+		Result:     result,
+	}
+
+	calculations = append(calculations, calc)
+	return c.JSON(http.StatusCreated, calc)
+}
 
 func main() {
-	fmt.Println("hey its calculator")
+	e := echo.New()
+
+	e.Use(middleware.CORS())
+	e.Use(middleware.Logger())
+
+	e.GET("/calculations", getCalculations)
+	e.POST("/calculations", postCalculations)
+	e.Start("localhost:8080")
 }
